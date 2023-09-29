c5303f40ded886b3d584f6e299afe5f6e2dd25a9
Shkumbini <shkumbinhajdari@gmail.com>
2023-09-29 19:55:49 +0200

    ushtrime me array dhe for

--- ushtrime.js
+++ ushtrime.js
@@ -22,3 +22,18 @@ console.log(fullName);
 let fullName1 = `${user.name} ${user.lastname} is ${user.age} years old from ${user.country.name}/${user.country.city.name}`;
 console.log(fullName1);
 
+
+function sum(a, b){
+    let shuma = a+b;
+    return shuma;
+}
+
+let result = sum(1, 4)
+console.log(result);
+
+
+module.exports = sum;
+
+
+
+
---
+++ ushtrime1.js
@@ -0,0 +1,51 @@
+// const  sum  = require('./ushtrime.js');
+// sum(1,4);
+
+
+// let arr=[]
+
+// for(let i=1; i<=10; i++){
+//     arr.push(i)
+// }
+// console.log(arr);
+
+
+// for(let i=1; i<=10; i++){
+//     arr.unshift(i)
+// }
+// console.log(arr);
+
+
+// for(let i=1; i<=10; i++){
+//     if(i<6){
+//         arr.push(i)
+//     }else{
+//         arr.unshift(i)
+//     }
+// }
+
+// console.log(arr);
+
+
+
+
+
+// for(let i in arr){
+//     if(arr[i] ==3){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+function detyra1(a, b){
+    let shuma = a+b;
+    return shuma;
+}
+
+console.log(detyra1(10, 9));
+
+
+
+module.exports = detyra1;
